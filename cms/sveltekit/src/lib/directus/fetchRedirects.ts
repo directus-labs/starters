438e6aa973c5ed792f941936df7e2246d82f0127
Lindsey Zylstra <lindseyzylstra@gmail.com>
2025-06-17 09:02:28 -0700

    Updates to filtering

--- cms/sveltekit/src/lib/directus/fetchRedirects.ts
+++ cms/sveltekit/src/lib/directus/fetchRedirects.ts
@@ -1,4 +1,3 @@
-// src/lib/fetchRedirects.ts
 import { PUBLIC_DIRECTUS_URL } from '$env/static/public';
 import { createDirectus, readItems, rest } from '@directus/sdk';
 
@@ -11,30 +10,54 @@ export interface SvelteRedirect {
 export async function fetchRedirects(): Promise<SvelteRedirect[]> {
 	const directusUrl = PUBLIC_DIRECTUS_URL;
 	if (!directusUrl) {
-		console.error('Missing PUBLIC_DIRECTUS_URL!');
+		console.warn('Missing PUBLIC_DIRECTUS_URL!');
 		return [];
 	}
 
 	try {
 		const directus = createDirectus(directusUrl).with(rest());
-		const items = await directus.request(
-			readItems('redirects', { fields: ['url_from', 'url_to', 'response_code'] })
+
+		const redirects = await directus.request(
+			readItems('redirects', {
+				filter: {
+					url_from: { _nnull: true },
+					url_to: { _nnull: true }
+				},
+				// Get all redirects (Directus defaults to 100 for limit)
+				limit: -1
+			})
 		);
 
-		return items
-			.filter(
-				(r): r is { url_from: string; url_to: string; response_code: '301' | '302' } =>
-					typeof r.url_from === 'string' &&
-					typeof r.url_to === 'string' &&
-					(r.response_code === '301' || r.response_code === '302')
-			)
-			.map((r) => ({
-				source: r.url_from,
-				destination: r.url_to,
-				permanent: r.response_code === '301'
-			}));
-	} catch (e) {
-		console.error('fetchRedirects error:', e);
+		const processedRedirects: SvelteRedirect[] = [];
+
+		for (const redirect of redirects) {
+			if (!redirect.url_from || !redirect.url_to) {
+				continue;
+			}
+
+			// If response code is not set, default to 301
+			let responseCode = redirect.response_code ? parseInt(redirect.response_code) : 301;
+
+			if (responseCode !== 301 && responseCode !== 302) {
+				responseCode = 301;
+			}
+
+			processedRedirects.push({
+				source: redirect.url_from,
+				destination: redirect.url_to,
+				permanent: responseCode === 301
+			});
+		}
+
+		console.info(`${redirects.length} redirects loaded`);
+
+		for (const redirect of redirects) {
+			console.info(`${redirect.response_code} - From: ${redirect.url_from} To:${redirect.url_to}`);
+		}
+
+		return processedRedirects;
+	} catch (error) {
+		console.error('Error loading redirects', error);
 		return [];
 	}
 }
